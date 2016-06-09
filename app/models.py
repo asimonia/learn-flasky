ed45e9c1ce44715649bf07900425e8a6525ef9ac
asimonia <alex.simonian@gmail.com>
2016-06-09 02:09:30 -0500

    Chapter 12: Followers in the application (12b)

--- app/models.py
+++ app/models.py
@@ -75,10 +75,10 @@ class User(UserMixin, db.Model):
     avatar_hash = db.Column(db.String(32))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     followed = db.relationship('Follow',
-                                foreign_keys=[Follow.follower_id],
-                                backref=db.backref('follower', lazy='joined'),
-                                lazy='dynamic',
-                                cascade='all, delete-orphan')
+                               foreign_keys=[Follow.follower_id],
+                               backref=db.backref('follower', lazy='joined'),
+                               lazy='dynamic',
+                               cascade='all, delete-orphan')
     followers = db.relationship('Follow',
                                 foreign_keys=[Follow.followed_id],
                                 backref=db.backref('followed', lazy='joined'),
@@ -207,13 +207,13 @@ class User(UserMixin, db.Model):
 
     def follow(self, user):
         if not self.is_following(user):
-            f = Follow(followed=user)
-            self.followed.append(f)
+            f = Follow(follower=self, followed=user)
+            db.session.add(f)
 
     def unfollow(self, user):
         f = self.followed.filter_by(followed_id=user.id).first()
         if f:
-            self.followed.remove(f)
+            db.session.delete(f)
 
     def is_following(self, user):
         return self.followed.filter_by(
--- notes/chapter_12.md
+++ notes/chapter_12.md
@@ -9,3 +9,10 @@ An association table will decompose the many-to-many relationship between two ta
 Followers are a many-to-many relationship.  The user can follow others and can be followed by other users.
 An association table called 'follows' takes care of this.
 
+
+
+Chapter 12: Followers in the application (12b)
+==============================================
+
+Implements a Follower button if user viewing is not a follower, or a unfollow button if the user is a follower.
+Also show follower and followed counts.
